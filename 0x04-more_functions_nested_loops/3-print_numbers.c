cd35d5797a95f0c7469c02417b1d654587015dae
Adegbasa Timothy <timothyadegbasa@gmail.com>
2022-06-10 13:19:14 -0700

    k

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,15 +1,13 @@
 #include "main.h"
 /**
- * print_numbers - function that checks for uppercase character.
- * Return: 0
+ * print_numbers - a function that prints the numbers, from 0 to 9,
+ * followed by a new line
+ * Return: Always 0
  */
 void print_numbers(void)
 {
-char c = '0';
-while (c <= '9'
-{
-_putchar(c);
-c++;
-}
+int i = '0';
+for (; i <= '9'; i++)
+_putchar(i);
 _putchar('\n');
 }
